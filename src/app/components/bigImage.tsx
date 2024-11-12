00bef64c70d33d1aac9e8dc41a0197cab39f15f4
Shakila <shakila.mehri.98@gmail.com>
2024-11-12 12:33:43 +0330

    updated bigImage

---
+++ src/app/components/bigImage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "../styles/bigImage.module.css";
+
+const BigImage = () => {
+  return (
+    <div className={styles.container}>
+      <img src="/images/slider1.jfif" alt="Image" />
+    </div>
+  )
+}
+
+export default BigImage
